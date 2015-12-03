36097f97825d221874decba5272b9af01c9a09b3
elam03 <elam03@gmail.com>
2015-12-03 09:21:28 -0800

    Improvements/enhancements * Added ngAnimate/angular-animate * Introducing some tabs * Testing carousal

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-    var app = angular.module('elam03.github.io.app', ['ui.bootstrap']);
+    var app = angular.module('elam03.github.io.app', ['ui.bootstrap', 'ngAnimate']);
 
     app.controller('ProjectListController', function($scope, $http) {
         $scope.oneAtATime = true;
@@ -16,11 +16,51 @@
                 {
                     var p = $scope.groups[i].previews[j];
 
-                    console.log('preview: ' + p);
+                    // console.log('preview: ' + p);
 
                     $scope.groups[i].previews[j] = preview_path + '/' + p;
                 }
             }
         });
+    });
+
+    app.controller('DropdownCtrl', function($scope) {
+        $scope.items = [
+            "The first choice!",
+            "And another choice for you.",
+            "but wait! A third!"
+        ];
+    });
+
+    app.controller('TabsController', function($scope, $window) {
+        $scope.tabs = [
+            { title:'Interests', content:'My interests...' },
+            // { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+        ];
+
+        $scope.alertMe = function() {
+            setTimeout(function() {
+                $window.alert('You\'ve selected the alert tab!');
+            });
+        };
+
+        $scope.tabs[0].active = true;
+    });
+
+    app.controller('CarouselDemoCtrl', function ($scope) {
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        $scope.addSlide = function() {
+        var newWidth = 600 + slides.length + 1;
+            slides.push({
+              image: '//placekitten.com/' + newWidth + '/300',
+              text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+                    ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+            });
+        };
+        for (var i=0; i<4; i++) {
+            $scope.addSlide();
+        }
     });
 })();
