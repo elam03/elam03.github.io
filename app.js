03bc19bbd573ed5eae59e3eae0068891fc78f5f5
elam03 <elam03@gmail.com>
2016-01-05 22:21:40 -0800

    Improvements/enhancements * Using a factory to load json data for my controllers now. * Cleaned up the slide carousel. * Added angular-touch for touch screen support.

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 (function() {
-    var app = angular.module('elam03.github.io.app', ['ui.bootstrap', 'ngAnimate']);
+    var app = angular.module('elam03.github.io.app', ['ui.bootstrap', 'ngAnimate', 'ngTouch']);
 
-    app.controller('ProjectListController', function($scope, $http) {
+    var project_data;
+
+    app.factory('ProjectFactory', function($q, $http) {
+
+        var projectList = [];
+
+        var service = {
+            getProjects: getProjects
+        };
+
+        return service;
+
+        function getProjects(refresh) {
+            var preview_path = '1gam_projects';
+            if (refresh || !this.projectList) {
+                return $http.get(preview_path + '/project_list.json').then(function(data) {
+                    this.projectList = data.data;
+
+                    // Adjust the image links so that they are correct.
+                    for (var i = 0; i < this.projectList.length; ++i) {
+                        for (var j = 0; j < this.projectList[i].previews.length; ++j) {
+                            this.projectList[i].previews[j] = preview_path + '/' + this.projectList[i].previews[j];
+                        }
+                    }
+
+                    return this.projectList;
+                })
+            } else {
+                var deferrer = $q.defer();
+                deferrer.resolve(rhis.projectList);
+                return deferrer.promise;
+            }
+        }
+    });
+
+    app.controller('ProjectListController', ["$scope", "$http", "ProjectFactory", function($scope, $http, ProjectFactory) {
         $scope.oneAtATime = true;
 
-        var preview_path = '1gam_projects';
-
-        $http.get(preview_path + '/project_list.json').then(function(res) {
-            $scope.groups = res.data;
-            // $scope.groups.previews[0] = 'blar';
-
-            for (var i = 0; i < $scope.groups.length; ++i)
-            {
-                for (var j = 0; j < $scope.groups[i].previews.length; ++j)
-                {
-                    var p = $scope.groups[i].previews[j];
-
-                    // console.log('preview: ' + p);
-
-                    $scope.groups[i].previews[j] = preview_path + '/' + p;
-                }
-            }
+        ProjectFactory.getProjects().then(function(projects){
+            $scope.projects = projects;
+        }, function(err){
+            console.log('uh oh!');
         });
-    });
+    }]);
 
     app.controller('DropdownCtrl', function($scope) {
         $scope.items = [
@@ -44,23 +66,36 @@
             });
         };
 
-        $scope.tabs[0].active = true;
+        // $scope.tabs[0].active = true;
     });
 
-    app.controller('CarouselDemoCtrl', function ($scope) {
+    app.controller('ProjectsCarouselCtrl', function ($http, $scope, ProjectFactory) {
+        ///////////////////////////////////////////////////////////////////////
+
         $scope.myInterval = 5000;
         $scope.noWrapSlides = false;
         var slides = $scope.slides = [];
-        $scope.addSlide = function() {
-        var newWidth = 600 + slides.length + 1;
+
+        $scope.addSlide = function(title, img, link) {
+            var newWidth = 600 + slides.length + 1;
             slides.push({
-              image: '//placekitten.com/' + newWidth + '/300',
-              text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-                    ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+                image: (!img) ? ('//placekitten.com/' + newWidth + '/300') : (img),
+                text: title,
+                link: link
             });
         };
-        for (var i=0; i<4; i++) {
-            $scope.addSlide();
-        }
+
+        ProjectFactory.getProjects().then(function(projects){
+            $scope.projects = projects;
+
+            for (var i = 0; i < projects.length; ++i) {
+                $scope.addSlide(projects[i].title, projects[i].previews[0], projects[i].content);
+                for (var j = 1; j < projects[i].previews.length; ++j) {
+                    $scope.addSlide(projects[i].title, projects[i].previews[j]);
+                }
+            }
+        }, function(err){
+            console.log('uh oh!');
+        });
     });
 })();
